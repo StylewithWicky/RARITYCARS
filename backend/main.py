04ddca2065910ab60db3446147242c37c2e388b8
Maitai <itsjustmaitai@gmail.com>
2026-02-02 17:54:27 +0300

    feat: integrate PostgreSQL with SQLModel and define Car schema

--- backend/main.py
+++ backend/main.py
@@ -3,10 +3,13 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Rarity Cars API")
 
+origins = [
+    "http://localhost:5173",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], 
+    allow_origins=origins, 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
